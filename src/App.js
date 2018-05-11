e8007c385465b192560217218eb22aba934aa6d4
shoaibbhimani <shoaibbhimani1392@gmail.com>
2018-05-12 02:19:00 +0530

    check horizontal and vertical complete

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import React, { Component } from "react";
 import "./App.css";
 import Cell from "./Cell";
 
+const defaultState = {
+  grid: [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+  ],
+  currentPlayer: "red",
+  showResult: false,
+  isActive: true
+};
+
 class App extends Component {
-  state = {
-    grid: [
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0]
-    ],
-    currentPlayer: "red"
-  };
+  state = defaultState;
 
   changePlayer = () => {
     this.setState({
@@ -24,7 +28,12 @@ class App extends Component {
   };
 
   seletGridCell = columnIndex => {
-    let column = this.state.grid[columnIndex];
+    if(!this.state.isActive){
+      return 
+    }
+
+    let grid = this.state.grid.slice();
+    let column = grid[columnIndex];
     let currentCellIndex = -1;
 
     column.forEach((p, i) => {
@@ -34,10 +43,82 @@ class App extends Component {
     });
 
     if (currentCellIndex >= 0) {
-      column[currentCellIndex] = this.state.currentPlayer;
+      grid[columnIndex][currentCellIndex] = this.state.currentPlayer;
 
-      this.changePlayer();
+      this.setState(
+        {
+          grid: grid
+        },
+        () => {
+          if (
+            this.isThereAnyWinner({ columnIndex, rowIndex: currentCellIndex })
+          ) {
+            this.setState({
+              showResult: true,
+              isActive: false
+            });
+          } else {
+            this.changePlayer();
+          }
+        }
+      );
     }
+  };
+
+  isThereAnyWinner = ({ columnIndex, rowIndex }) => {
+    if (
+      this.checkIsThereHorizontalMatch({ rowIndex }) ||
+      this.checkIsVerticalMatch({ columnIndex })
+    ) {
+      return true;
+    }
+
+    return false;
+  };
+
+  checkIsThereHorizontalMatch = ({ rowIndex }) => {
+    let found = 0;
+
+    this.state.grid.forEach((gridItem, index) => {
+      if (gridItem[rowIndex] === this.state.currentPlayer) {
+        found++;
+      } else {
+        if (found !== 4) found = 0;
+      }
+    });
+
+    return found >= 4;
+  };
+
+  checkIsVerticalMatch = ({ columnIndex }) => {
+    let found = 0;
+    const columnGrid = this.state.grid[columnIndex];
+    columnGrid.map(columnItem => {
+      if (columnItem === this.state.currentPlayer) {
+        found++;
+      } else {
+        if (found !== 4) found = 0;
+      }
+    });
+
+    return found >= 4;
+  };
+
+  resetGrid = () => {
+    this.setState({
+      grid: [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+      ],
+      currentPlayer: "red",
+      showResult: false,
+      isActive: true
+    });
   };
 
   render() {
@@ -59,8 +140,18 @@ class App extends Component {
         </div>
       );
     });
-    return <div className="App">{cells}</div>;
+    return (
+      <div className="App">
+        <div>{cells} </div>
+        {this.state.showResult ? (
+          <div className="winner-result">
+            WINNNER is {this.state.currentPlayer}
+            <button onClick={this.resetGrid}>Reset Button</button>
+          </div>
+        ) : null}
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
